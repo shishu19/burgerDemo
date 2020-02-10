434f4a0e7b261f419d229afc69bd0460a7abd09b
shashankrgupta <shashank.r.gupta@accenture.com>
2020-02-10 16:43:15 +0530

    Add Con

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
+class BurgerBuilder extends Component {
+    /* constructor(props) {
+        super(props)
+        this.state = {...}
+    } */
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients;
+        const updateCounted = oldCount + 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updateCounted;
+        const priceAddition = INGREDENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients(type);
+        if (oldCount <= 0){
+            return;
+        }
+        const updateCounted = oldCount - 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updateCounted;
+        const priceAddition = INGREDENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    }
+    
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler} 
+                    ingredientRemoved={this.removeIngredientHandler} />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
